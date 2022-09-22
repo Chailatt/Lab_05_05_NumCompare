1cc6f02c9023a64af152ede18a97dc9fa202504b
Chevez Robertson <chai@dhcp-172-30-225-209.guest.uc.edu>
2022-09-22 15:19:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+        int num1 = 15;
+        int num2 = 16;
+        String word = "hi";
+
+        if (num1 == num2){
+            System.out.println("numbers are equal"); }
+        else if (num1 != num2){
+            System.out.println("One number is less"); }
+        else {
+            System.out.println("that's not a number, try again.");
+        }
+    }
+}
